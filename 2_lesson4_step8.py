e4ba17f99aaea9763d690e150a0053d06d6f769b
Gleb <monachusgl@gmail.com>
2022-06-06 23:43:55 +0300

    Задача на использование WebDriverWait и expected_conditions.

---
+++ 2_lesson4_step8.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    WebDriverWait(browser, 5).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    browser.find_element(By.ID, "book").click()
+    answer = calc(browser.find_element(By.ID, "input_value").text)
+    browser.find_element(By.ID, "answer").send_keys(answer)
+    browser.find_element(By.ID, "solve").click()
+except Exception as errors:
+    print(f"Errors in this scope {errors}")
+finally:
+    time.sleep(10)
+    browser.quit()
